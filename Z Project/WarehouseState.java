1631422ff80e5d3cbc30132b35947447beb9c9e8
Henrywalker2002 <davisdavis448@gmail.com>
2024-04-19 18:50:15 +0700

    init

--- Z Project/WarehouseState.java
+++
@@ -1,8 +0,0 @@
-public abstract class WarehouseState {
-    protected static WarehouseContext context;
-    protected WarehouseState() {
-        //context = LibContext.instance();
-    }
-    public abstract void run();
-}
-
